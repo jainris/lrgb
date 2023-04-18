53ce6454cbdf1e3dd4b3aff90f11137ee7a124b3
Rishabh Jain <rj412@cam.ac.uk>
2023-04-18 15:16:38 +0100

    Added mem_gnn_2, which removes the linear layers before and after the mem module.

--- graphgps/layer/mem_processors.py
+++ graphgps/layer/mem_processors.py
@@ -140,3 +140,125 @@ class MemoryGCNConv(GCNConv):
 
     def message_and_aggregate(self, adj_t: SparseTensor, x: Tensor) -> Tensor:
         raise NotImplementedError
+
+
+class MemoryGCNConv_2(GCNConv):
+    def __init__(
+        self,
+        in_channels: int,
+        out_channels: int,
+        memory_module: MemoryModule,
+        nb_z_fts: int,
+        mem_msg_fts: int,
+        improved: bool = False,
+        cached: bool = False,
+        add_self_loops: bool = True,
+        normalize: bool = True,
+        bias: bool = False,
+        send_to_all: bool = False,
+        **kwargs
+    ):
+        super().__init__(
+            in_channels,
+            out_channels,
+            improved,
+            cached,
+            add_self_loops,
+            normalize,
+            bias,
+            **kwargs
+        )
+        self.memory_module = memory_module
+        self.send_to_all = send_to_all
+
+    def forward(
+        self,
+        batch: Batch,
+        edge_weight: OptTensor = None,
+        prev_mem_state: Optional[MemoryState] = None,
+    ) -> Tuple[Batch, MemoryState]:
+        x = batch.x
+        edge_index = batch.edge_index
+        if self.normalize:
+            if isinstance(edge_index, Tensor):
+                cache = self._cached_edge_index
+                if cache is None:
+                    edge_index, edge_weight = gcn_norm(  # yapf: disable
+                        edge_index,
+                        edge_weight,
+                        x.size(self.node_dim),
+                        self.improved,
+                        self.add_self_loops,
+                    )
+                    if self.cached:
+                        self._cached_edge_index = (edge_index, edge_weight)
+                else:
+                    edge_index, edge_weight = cache[0], cache[1]
+
+            elif isinstance(edge_index, SparseTensor):
+                cache = self._cached_adj_t
+                if cache is None:
+                    edge_index = gcn_norm(  # yapf: disable
+                        edge_index,
+                        edge_weight,
+                        x.size(self.node_dim),
+                        self.improved,
+                        self.add_self_loops,
+                    )
+                    if self.cached:
+                        self._cached_adj_t = edge_index
+                else:
+                    edge_index = cache
+
+        x = self.lin(x)
+
+        z = batch.x
+        read_values, msg_recepients, next_mem_state = self.memory_module(
+            z=z, batch=batch.batch, prev_state=prev_mem_state
+        )
+
+        # propagate_type: (x: Tensor, edge_weight: OptTensor)
+        out = self.propagate(
+            edge_index,
+            x=x,
+            edge_weight=edge_weight,
+            read_values=read_values,
+            msg_recepients=msg_recepients,
+            nb_nodes=x.shape[0],
+            size=None,
+        )
+
+        if self.bias is not None:
+            out += self.bias
+
+        batch.x = out
+
+        return batch, next_mem_state
+
+    def aggregate(
+        self,
+        inputs: Tensor,
+        index: Tensor,
+        read_values: Tensor,
+        msg_recepients: Tensor,
+        nb_nodes: int,
+        ptr: Optional[Tensor] = None,
+        dim_size: Optional[int] = None,
+    ) -> Tensor:
+        if ptr is not None:
+            raise ValueError("Sparse tensors are not supported yet.")
+        else:
+            inputs, index = update_using_memory(
+                msgs=inputs,
+                index=index,
+                read_values=read_values,
+                message_recepients=msg_recepients,
+                nb_nodes=nb_nodes,
+                send_to_all=self.send_to_all,
+            )
+            return scatter(
+                inputs, index, dim=self.node_dim, dim_size=dim_size, reduce=self.aggr
+            )
+
+    def message_and_aggregate(self, adj_t: SparseTensor, x: Tensor) -> Tensor:
+        raise NotImplementedError
--- graphgps/network/memory_module_gnn.py
+++ graphgps/network/memory_module_gnn.py
@@ -7,6 +7,7 @@ from torch_geometric.graphgym.register import register_network
 from torch_geometric.graphgym.init import init_weights
 
 from graphgps.layer.mem_processors import MemoryGCNConv
+from graphgps.layer.mem_processors import MemoryGCNConv_2
 from graphgps.layer.memory_module import memory_module_factory
 
 
@@ -74,3 +75,69 @@ class MemoryGNN(torch.nn.Module):
 
 
 register_network("mem_gnn", MemoryGNN)
+
+
+class MemoryGNN2(torch.nn.Module):
+    """
+    General GNN model: encoder + stage + head
+    Args:
+        dim_in (int): Input dimension
+        dim_out (int): Output dimension
+        **kwargs (optional): Optional additional args
+    """
+
+    def __init__(self, dim_in, dim_out, **kwargs):
+        super().__init__()
+        GNNHead = register.head_dict[cfg.gnn.head]
+
+        self.encoder = FeatureEncoder(dim_in)
+        dim_in = self.encoder.dim_in
+
+        self.pre_mp = None
+        if cfg.gnn.layers_pre_mp > 0:
+            self.pre_mp = GNNPreMP(dim_in, cfg.gnn.dim_inner, cfg.gnn.layers_pre_mp)
+            dim_in = cfg.gnn.dim_inner
+
+        gnn_layers = []
+
+        memory_module = memory_module_factory(
+            memory_module=cfg.memory.module,
+            output_size=cfg.memory.output_size,
+            embedding_size=cfg.memory.embedding_size,
+            memory_size=cfg.memory.memory_size,
+            nb_heads=cfg.memory.nb_heads,
+            aggregation_technique=cfg.memory.aggregation_technique,
+            nb_z_fts=cfg.memory.nb_z_fts,
+        )
+
+        for _ in range(cfg.gnn.layers_mp):
+            gnn_layers.append(
+                MemoryGCNConv_2(
+                    in_channels=dim_in,
+                    out_channels=cfg.gnn.dim_inner,
+                    memory_module=memory_module,
+                    nb_z_fts=cfg.memory.nb_z_fts,
+                    mem_msg_fts=cfg.memory.output_size,
+                    send_to_all=cfg.memory.send_to_all,
+                )
+            )
+            dim_in = cfg.gnn.dim_inner
+
+        self.gnn_layers = torch.nn.ModuleList(gnn_layers)
+
+        self.post_mp = GNNHead(dim_in=cfg.gnn.dim_inner, dim_out=dim_out)
+
+        self.apply(init_weights)
+
+    def forward(self, batch):
+        batch = self.encoder(batch)
+        if self.pre_mp is not None:
+            batch = self.pre_mp(batch)
+        mem_state = None
+        for module in self.gnn_layers:
+            batch, mem_state = module(batch=batch, prev_mem_state=mem_state)
+        batch = self.post_mp(batch)
+        return batch
+
+
+register_network("mem_gnn_2", MemoryGNN2)
